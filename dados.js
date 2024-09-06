ff815f8cea2084867f014c30a5e0abcfd7bb06be
Janyson Marques <janyson29@hotmail.com>
2024-09-06 18:12:57 -0300

    aula 3

---
+++ dados.js
@@ -0,0 +1,27 @@
+let dados = [
+  {
+    titulo : "Rebeca Andrade",
+    descricao : "Rebeca Rodrigues de Andrade é uma ginasta artística brasileira. Ela é a primeira ginasta brasileira a conquistar uma medalha olímpica na ginástica artística feminina.",
+    link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    redeSocial : "https://www.instagram.com/rebecarandrade/",
+  },
+  {
+    titulo : "Rayssa Leal",
+    descricao : "Rayssa Leal, conhecida como a Fadinha do Skate, é uma skatista profissional brasileira. Ela é a mais jovem medalhista olímpica do Brasil, com apenas 13 anos.",
+    link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    redeSocial : "https://www.instagram.com/rayssalealsk8/",
+  },
+  {
+    titulo : "Julia Soares",
+    descricao: "Julia Soares é uma jovem ginasta artística brasileira, destaque nas competições internacionais. Com suas apresentações de alto nível e habilidade técnica, ela representa o futuro promissor da ginástica artística do Brasil.",
+    link: "https://pt.wikipedia.org/wiki/J%C3%BAlia_Soares",
+    redeSocial: "https://www.instagram.com/juju_bsoares/",
+  },
+  {
+    titulo : "Italo Ferreira",
+    descricao : "Ítalo Ferreira é um surfista profissional brasileiro. Ele é o primeiro surfista brasileiro a conquistar uma medalha de ouro nas Olimpíadas.",
+    link : "https://pt.wikipedia.org/wiki/%C3%8Dtalo_Ferreira",
+    redeSocial : "https://www.instagram.com/italoferreira/",
+  },
+];
+
