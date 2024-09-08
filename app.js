f61b72756e67c077bec99b67d8472acd25fa8f63
Janyson Marques <janyson29@hotmail.com>
2024-09-08 16:31:23 -0300

    aula 5

--- app.js
+++ app.js
@@ -2,14 +2,40 @@ function pesquisar() {
   // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
   let section = document.getElementById("resultados-pesquisa");
 
+  // Obtém o valor digitado pelo usuário no campo de pesquisa
+  let campoPesquisa = document.getElementById("campo-pesquisa").value.trim();
+  console.log(campoPesquisa);
+
+  // Se campoPesquisa for uma string sem nada
+  if (!campoPesquisa) {
+    // Limpa a seção de resultados
+    section.innerHTML = "<p>Nenhum resultado encontrado. Você precisa digitar o nome de um atleta ou esporte.</p>";
+    return;
+  }
+
+  campoPesquisa = campoPesquisa.toLowerCase();
+
   // Inicializa uma string vazia para armazenar os resultados da pesquisa
   let resultados = "";
 
+  let titulo = "";
+  let descricao = "";
+  let tags = "";
+
   // Itera sobre cada dado da pesquisa
   for (let dado of dados) {
-    // Constrói o HTML para cada item do resultado da pesquisa,
-    // incluindo título, descrição, link para a rede social e link para mais informações
-    resultados += `
+    // Converte o título e a descrição para letras minúsculas
+    // para que a pesquisa seja case-insensitive
+    titulo = dado.titulo.toLowerCase();
+    descricao = dado.descricao.toLowerCase();
+    tags = dado.tags.toLowerCase();
+
+    // Verifica se o campo de pesquisa está contido no título do dado
+    // e exibe o resultado apenas se o campo de pesquisa for encontrado
+    // no título do dado
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+      // cria um novo elemento HTML
+      resultados += `
       <div class="item-resultado">
         <h2>
           <a href="${dado.redeSocial}" target="_blank">${dado.titulo}</a>
@@ -18,6 +44,24 @@ function pesquisar() {
         <a href="${dado.link}" target="_blank">Mais informações</a>
       </div>
     `;
+    }
+
+    // Constrói o HTML para cada item do resultado da pesquisa,
+    // incluindo título, descrição, link para a rede social e link para mais informações
+    // resultados += `
+    //   <div class="item-resultado">
+    //     <h2>
+    //       <a href="${dado.redeSocial}" target="_blank">${dado.titulo}</a>
+    //     </h2>
+    //     <p class="descricao-meta">${dado.descricao}</p>
+    //     <a href="${dado.link}" target="_blank">Mais informações</a>
+    //   </div>
+    // `;
+  }
+
+  // Se nenhum resultado for encontrado, exibe uma mensagem
+  if (!resultados) {
+    resultados = "<p>Nenhum resultado encontrado</p>";
   }
 
   // Atribui o HTML gerado para a seção de resultados
--- dados.js
+++ dados.js
@@ -4,24 +4,42 @@ let dados = [
     descricao : "Rebeca Rodrigues de Andrade é uma ginasta artística brasileira. Ela é a primeira ginasta brasileira a conquistar uma medalha olímpica na ginástica artística feminina.",
     link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
     redeSocial : "https://www.instagram.com/rebecarandrade/",
+    tags: "ginasta, ginástica, artística, medalhista, olímpica, brasileira",
   },
   {
     titulo : "Rayssa Leal",
     descricao : "Rayssa Leal, conhecida como a Fadinha do Skate, é uma skatista profissional brasileira. Ela é a mais jovem medalhista olímpica do Brasil, com apenas 13 anos.",
     link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
     redeSocial : "https://www.instagram.com/rayssalealsk8/",
+    tags: "skatista, profissional, medalhista, olímpica, brasileira",
   },
   {
     titulo : "Julia Soares",
     descricao: "Julia Soares é uma jovem ginasta artística brasileira, destaque nas competições internacionais. Com suas apresentações de alto nível e habilidade técnica, ela representa o futuro promissor da ginástica artística do Brasil.",
     link: "https://pt.wikipedia.org/wiki/J%C3%BAlia_Soares",
     redeSocial: "https://www.instagram.com/juju_bsoares/",
+    tags: "ginasta, ginástica, artística, brasileira",
   },
   {
     titulo : "Italo Ferreira",
     descricao : "Ítalo Ferreira é um surfista profissional brasileiro. Ele é o primeiro surfista brasileiro a conquistar uma medalha de ouro nas Olimpíadas.",
     link : "https://pt.wikipedia.org/wiki/%C3%8Dtalo_Ferreira",
     redeSocial : "https://www.instagram.com/italoferreira/",
+    tags: "surfista, profissional, medalhista, olímpico, brasileiro",
+  },
+  {
+    titulo : "Gabriel Medina",
+    descricao : "Gabriel Medina é um surfista profissional brasileiro, bicampeão mundial de surf. Ele é conhecido por seu estilo agressivo e manobras inovadoras nas ondas.",
+    link : "https://pt.wikipedia.org/wiki/Gabriel_Medina",
+    redeSocial : "https://www.instagram.com/gabrielmedina/",
+    tags: "surfista, profissional, bicampeão, mundial, brasileiro",
+  },
+  {
+    titulo : "Ana Marcela Cunha",
+    descricao : "Ana Marcela Cunha é uma nadadora de águas abertas brasileira, considerada uma das melhores do mundo em sua modalidade. Ela já conquistou diversos títulos e medalhas em competições internacionais.",
+    link : "https://pt.wikipedia.org/wiki/Ana_Marcela_Cunha",
+    redeSocial : "https://www.instagram.com/anamarcela92/",
+    tags: "nadadora, águas, abertas, brasileira",
   },
 ];
 
